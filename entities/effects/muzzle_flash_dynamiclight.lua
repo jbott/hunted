366b772e7179962055c00f40a6af521f8b343269
John (SnowLeopardJB) <snowleopardjb@gmail.com>
2014-07-14 12:49:08 -0400

    Muzzle Flash changes for color and duration

--- entities/effects/muzzle_flash_dynamiclight.lua
+++ entities/effects/muzzle_flash_dynamiclight.lua
@@ -6,12 +6,12 @@ function EFFECT:Init( data )
       if (light) then
          light.Pos = data:GetOrigin()
          light.r = 255
-         light.g = 255
-         light.b = 255
+         light.g = 191
+         light.b = 39
          light.Brightness = 4
          light.Size = 250
-         light.Decay = 0.1
-         light.DieTime = CurTime() + 0.01
+         light.Decay = 0.01
+         light.DieTime = CurTime() + 0.001
          light.Style = 0
       end
    end
