baf061a8a380356c78ca49795c042f95bbf31f53
John (SnowLeopardJB) <snowleopardjb@gmail.com>
2014-07-15 19:13:36 -0400

    Add floating player names

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -13,6 +13,39 @@ function GM:HUDPaint()
 		surface.SetTextPos(10, 10)
 		surface.DrawText(CLASS.GetClassName(LocalPlayer():Class()))
 	end
+
+	for _,ply in pairs(player.GetAll()) do
+		paintPlayerName(ply)
+	end
+end
+
+function paintPlayerName(ply)
+	if (!ply:Alive()) then return end
+	if (ply:Team() == TEAM_UNASSIGNED ||
+		ply:Team() == TEAM_SPEC) then return end
+	if (ply == LocalPlayer()) then return end
+	local dist = LocalPlayer():GetPos():Distance(ply:GetPos())
+	if (dist > 255) then return end
+
+	local text = ply:GetName()
+	local font = "DermaLarge"
+	surface.SetFont(font)
+
+	local pos = (ply:GetPos() + Vector(0, 0, 70)):ToScreen()
+	local w, h = surface.GetTextSize(text)
+
+	if pos.visible then
+		local x = pos.x
+		local y = pos.y
+
+		x = x - w / 2
+		y = y - h
+
+		-- The fonts internal drop shadow looks lousy with AA on
+		draw.SimpleText( text, font, x+1, y+1, Color(0,0,0,120) )
+		draw.SimpleText( text, font, x+2, y+2, Color(0,0,0,50) )
+		draw.SimpleText( text, font, x, y, Color(255,255,255,255 - dist))
+	end
 end
 
 function GM:HUDShouldDraw( name )
@@ -20,4 +53,8 @@ function GM:HUDShouldDraw( name )
         return false
     end
     return true
+end
+
+-- Disable overlay
+function GM:HUDDrawTargetID()
 end
